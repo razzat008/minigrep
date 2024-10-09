925a9e200baf2da61700fdb9fc9f42cd089a4625
Razzat <radiusrazzat111@gmail.com>
2024-10-09 21:38:59 +0545

    defactoring stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-use std::env;
+use mini_grep::Config;
+use std::{env, process};
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let args: Vec<String> = env::args().collect(); // collecting all arguments into a vector of
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing argument: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {:?}", config.query);
+    println!("In file `{:?}`", config.file_path);
+    println!("================");
+
+    if let Err(e) = mini_grep::run(config) {
+        println!("Application crashed {}", e);
+        process::exit(1);
+    }
 }
